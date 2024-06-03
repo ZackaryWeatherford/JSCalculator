6a1627523543ed29ad4c9482e1b2d79922596fd0
zackary3422 <81474358+zackary3422@users.noreply.github.com>
2024-06-03 18:00:13 -0400

    Gotten more JS running for calculator. Still working on adding math to input field

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,50 @@
 
-
 let prevInput;
-let currentInput;
-
-let leftNum;
-let operator;
-let rightNum;
-
+let currentInput = '';
 
 
 function getButtonInput(button){
 
+    let buttonText = button.textContent;
+
+    if(buttonText !== 'C' && buttonText !== '=' && currentInput.length < 23){
     
+        resize();
+
+        if(buttonText === '%' && !Number.isInteger(Number(currentInput.charAt(currentInput.length-2)))){
+            console.log("TEST");
+            return;
+        }
+
+
+        currentInput += buttonText;
+
+        let inputField = document.querySelector("#input-field");
+        inputField.innerHTML = currentInput;
+    }
+
+
+    switch(buttonText){
+        case 'C':
+            currentInput = '';
+            document.querySelector("#input-field").innerHTML = "0";
+            document.querySelector("#previous-field").innerHTML = "&nbsp;"; 
+            break;
+        case '=':
+            calculate();
+            break;
+    }
+
+
+}
+
+function calculate(){
+
+}
+
+function resize(button){
+
+    if(currentInput.length > 15)
+        document.getElementById("input-field").style.fontSize = "30px";
 
 }
